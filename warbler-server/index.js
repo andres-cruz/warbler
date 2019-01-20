82fb3a6e032d1c2ac598c4914acd0b79daea2abd
Andres <andrescruz.tech@gmail.com>
2019-01-20 18:43:21 -0300

    Hashing and storing passwords

--- warbler-server/index.js
+++ warbler-server/index.js
@@ -1,19 +1,26 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorHandler = require('./handlers/error');
 
+// requiring routes
+const authRoutes = require('./routes/auth');
 
 // settings
 const app = express();
 const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use((req, res, next) => {
     let err = new Error('Not found.')
     err.status = 404;
     next(err);
 });
+
+// using routes
+app.use('/api/auth', authRoutes);
 
 app.use(errorHandler);
 
