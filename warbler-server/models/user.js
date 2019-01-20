07c8e309d69e45484722850d2bcfaf0da88f05d0
Andres <andrescruz.tech@gmail.com>
2019-01-20 17:34:49 -0300

    Added user middleware

--- warbler-server/models/user.js
+++ warbler-server/models/user.js
@@ -21,6 +21,28 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.pre('save', async function() {
+    try {
+        if(!this.isModified('password')) {
+            return next();
+        }
+        let hashedPassword = bcrypt.hash(this.password, 10);
+        this.password = hashedPassword;
+        return next();
+    } catch(err) {
+        next(err);
+    }
+});
+
+userSchema.method.comparePassword = async function(cadidatePassword, next) {
+        try {
+            let isMatch = await bcrypt.compare(cadidatePassword, this.password);
+            return isMatch;
+        } catch (err) {
+            return next(err);
+        }
+};
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
